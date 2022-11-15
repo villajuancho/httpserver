075a727917de7ef58b75934f60526a059cd546c7
villajuancho <villa.juancho@gmail.com>
2022-11-15 16:05:05 -0500

    bb

---
+++ nginx/public/k8s/config/docker/install.sh
@@ -0,0 +1,6 @@
+dpkg -i gnupg_2.2.19-3ubuntu2.1_all.deb
+dpkg -i containers-common_1-22_all.deb
+dpkg -i containerd.io_1.6.6-1_amd64.deb
+dpkg -i docker-ce-cli_20.10.17~3-0~ubuntu-focal_amd64.deb
+dpkg -i docker-ce_20.10.17~3-0~ubuntu-focal_amd64.deb
+dpkg -i docker-compose-plugin_2.6.0~ubuntu-focal_amd64.deb
