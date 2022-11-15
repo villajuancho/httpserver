a3c3cf80805a5f16993dfc85ffc5ef08db2fc7b4
villajuancho <villa.juancho@gmail.com>
2022-11-15 15:44:15 -0500

    sasd

--- nginx/public/k8s/apt/focal/focal.sh
+++
@@ -1,1 +0,0 @@
-cat focal.tar.gz.* | tar xzvf -
---
+++ nginx/public/k8s/apt/focal/focalGit.sh
@@ -0,0 +1,5 @@
+curl -k -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/apt/focal/focal.tar.gz.aa
+curl -k -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/apt/focal/focal.tar.gz.ab
+curl -k -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/apt/focal/focal.tar.gz.ac
+curl -k -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/apt/focal/focal.tar.gz.ad
+cat focal.tar.gz.* | tar xzvf -
