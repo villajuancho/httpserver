fa5ba32fe33f183f034b5b97a60ad7d6d23e33b6
villajuancho <villa.juancho@gmail.com>
2022-11-23 16:30:50 -0500

    mock

--- nginx/public/k8s/config/configGit.sh
+++ nginx/public/k8s/config/configGit.sh
@@ -10,3 +10,6 @@ curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/pu
 curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/metrics.tar
 curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/logging.tar
 curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/config.yaml
+curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/mockapp.tar.gz.aa
+curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/mockapp.tar.gz.ab
+
