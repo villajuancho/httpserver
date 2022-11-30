1288923f5ec0e7f3f0b5cc984e2057b3e68ff414
villajuancho <villa.juancho@gmail.com>
2022-11-29 22:24:23 -0500

    others

--- nginx/public/k8s/config/configGit.sh
+++ nginx/public/k8s/config/configGit.sh
@@ -11,3 +11,22 @@ curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/pu
 curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/logging.tar
 curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/config.yaml
 curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/mockapp.tar
+curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/uploadfiles.sh
+
+curl -u "admin:passw0rd" --upload-file "https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/config.yaml" "http://192.168.88.249:8099/repository/raw1/config/config.yaml"
+
+
+
+
+curl -LO https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/config.yaml
+
+
+
+curl -u "admin:passw0rd" -H "Content-Type: multipart/form-data" --data-binary "@./containerd.io_1.6.6-1_amd64.deb" "http://$NEXUSAPT/repository/apt-focal/"
+
+curl -u "admin:passw0rd" -H "Content-Type: multipart/form-data" --data-binary "@./mockapp.tar" "http://192.168.88.249:8099/repository/raw1/config/config.yaml"
+
+curl -u "admin:passw0rd" -o "https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/config.yaml" "http://192.168.88.249:8099/repository/raw1/config/config.yaml"
+
+
+curl -v -u "admin:passw0rd" --upload-file "https://raw.githubusercontent.com/villajuancho/httpserver/main/nginx/public/k8s/config/config.yaml" "http://192.168.88.249:8099/repository/raw1/config/config.yaml"
